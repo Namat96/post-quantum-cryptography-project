b898f02bf36dbe84acd6e58a76feeaeaceb67e47
Namat Ullah <139422672+Namat96@users.noreply.github.com>
2026-02-18 13:16:47 +0500

    Day 2: Add vector operations

---
+++ week1_foundations/vectors.py
@@ -0,0 +1,18 @@
+# Day 2 - Vector Operations
+# Author: Namat
+
+import numpy as np
+
+v1 = np.array([2, 3])
+v2 = np.array([4, 1])
+
+# Vector addition
+v_add = v1 + v2
+
+# Dot product
+dot = np.dot(v1, v2)
+
+print("Vector 1:", v1)
+print("Vector 2:", v2)
+print("Addition:", v_add)
+print("Dot Product:", dot)
